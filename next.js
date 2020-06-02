46652dd81f3e3e5b9b86a886a59d26ddab34136e
yali.zhang <yali.zhang@shuyun.com>
2020-06-02 15:27:39 +0800

    add production mode to package.json

--- next.js
+++ next.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const NEXT = require('next');
-
+console.log("process.env.NODE_ENV", process.env.NODE_ENV)
 const client = NEXT({
-    dev: process.env.NODE_ENV !== 'production',
+    dev: (!process.env.NODE_ENV || process.env.NODE_ENV === "develop"),
     dir: './app'
 });
 

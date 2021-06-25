53acc681a8e21fe4a09bc86d78b699097ae08b7c
zhoushoujian <863165094@qq.com>
2021-06-25 22:15:09 +0800

    fix: 修复goujian

--- app/next.config.js
+++ app/next.config.js
@@ -1,8 +1,14 @@
-const withLess = require('@zeit/next-less')
-module.exports = withLess()
-// module.exports = withLess({
-//   cssModules: true,
-//   webpack: function (config) {
-//     return config;
-//   }
-// });
+/** @format */
+
+const withLess = require("@zeit/next-less");
+const withCss = require("@zeit/next-css");
+const withPlugins = require("next-compose-plugins");
+const path = require("path");
+
+module.exports = withPlugins([withLess, withCss], {
+  cssModules: false,
+  cssLoaderOptions: {
+    camelCase: true,
+    localIdentName: "[local]___[hash:base64:5]",
+  },
+});
